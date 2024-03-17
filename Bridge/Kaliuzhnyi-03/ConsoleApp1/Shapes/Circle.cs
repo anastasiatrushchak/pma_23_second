cb5136b802e14dd65aca4efbcfc83b5810b209a8
Oleh911 <150347416+Oleh911@users.noreply.github.com>
2024-03-17 23:09:33 +0200

    KO-03 Bridge
    KO-03 Bridge

---
+++ Bridge/Kaliuzhnyi-03/ConsoleApp1/Shapes/Circle.cs
@@ -0,0 +1,51 @@
+﻿using Bridge.Colors;
+
+namespace Bridge.Shapes;
+
+public class Circle : Shape
+{
+    private readonly double _radius;
+    public Circle(double radius, IColor color) : base(color)
+    {
+        if (radius <= 0)
+        {
+            throw new ArgumentException("Error! Radius can't be smaller or equal to zero!");
+        }
+        _radius = radius;
+    }
+
+    public override double Area()
+    {
+        return Math.Sqrt(_radius) * Math.PI;
+    }
+
+    public double Area(int roundPNumber)
+    {
+        if (roundPNumber < 0 || roundPNumber > 16)
+        {
+            throw new ArgumentException("Error! Rounding up is possible in range 0-16!");
+        }
+        return Math.Sqrt(_radius) * Math.Round(Math.PI, roundPNumber);
+    }
+
+    public override double Perimeter()
+    {
+        return 2 * _radius * Math.PI;
+    }
+
+    public double Perimeter(int roundPNumber)
+    {
+        if (roundPNumber < 0 || roundPNumber > 16)
+        {
+            throw new ArgumentException("Error! Rounding up is possible in range 0-16!");
+        }
+        return 2 * _radius * Math.Round(Math.PI, roundPNumber);
+    }
+
+    public override string ToString()
+    {
+        string circleToString = $"There is circle with radius = {_radius} and {Color} color";
+
+        return circleToString;
+    }
+}
---
+++ Bridge/Kaliuzhnyi-03/ConsoleApp1/Shapes/Rectangle.cs
@@ -0,0 +1,42 @@
+﻿using Bridge.Colors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge.Shapes;
+public class Rectangle : Shape
+{
+    protected readonly double _sideOne;
+    protected readonly double _sideTwo;
+
+    public Rectangle(double sideOne, double sideTwo, IColor color) : base(color)
+    {
+        if (sideOne <= 0 || sideTwo <= 0)
+        {
+            throw new ArgumentException("Error! Rectangle's side can't be smaller or equal to zero!");
+        }
+
+        _sideOne = sideOne;
+        _sideTwo = sideTwo;
+    }
+
+    public override double Area()
+    {
+        return _sideOne * _sideTwo;
+    }
+
+    public override double Perimeter()
+    {
+        return (_sideOne + _sideTwo) * 2;
+    }
+
+    public override string ToString()
+    {
+        string rectangleToString = $"There is rectangle with sides: {_sideOne}, {_sideTwo}; color: {Color}";
+
+        return rectangleToString;
+    }
+
+}
