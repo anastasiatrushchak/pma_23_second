bb2f322130f82c9d41458d121115230663b76734
Zlatomyra <133869874+Zlatomyra@users.noreply.github.com>
2024-03-17 12:38:07 +0200

    ZS-03 bridge

---
+++ Bridge/Severylova-03/Bridge/Bridge/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Shape restangle = new Rectangle(5, 10, new Red());
+                Console.WriteLine(restangle);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                Shape circle = new Circle(8, new Purple());
+                Console.WriteLine(circle);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                Shape triangle = new Triangle(4, 6, 8, new Green());
+                Console.WriteLine(triangle);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                Shape square = new Square(5, new Blue());
+                Console.WriteLine(square);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
