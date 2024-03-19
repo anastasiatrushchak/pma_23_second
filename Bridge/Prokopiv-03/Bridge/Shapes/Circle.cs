7c916ea8371258391abde1c957354fa27ed53714
OlesProkopiv <143633603+OlesProkopiv@users.noreply.github.com>
2024-03-19 19:27:11 +0200

    PO-03 Bridge

---
+++ Bridge/Prokopiv-03/Bridge/Shapes/Circle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge.Shapes
+{
+    public class Circle : Shape
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            if (radius < 0)
+                throw new ArgumentException("The radius must be non-negative");
+            this.radius = radius;
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * radius * radius;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * Math.PI * radius;
+        }
+    }
+}
