e193df85686a38ca38855de76e9718f44a9cf2d2
anastasiatrushchak <143631657+anastasiatrushchak@users.noreply.github.com>
2024-03-19 20:14:24 +0200

    TA-03 Bridge

---
+++ Bridge/Trushchak-03/Bridge/Bridge/Rectangle.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    class Rectangle : Shape
+    {
+        private double length;
+        private double width;
+
+        public Rectangle(double length, double width, IColor color) : base(color)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+        public override double GetArea()
+        {
+            try
+            {
+                if (length <= 0 || width <= 0)
+                    throw new ArgumentException("Sides of a rectangle must be positive.");
+
+                return length * width;
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Error calculating area: {ex.Message}");
+                return 0;
+            }
+        }
+
+        public override double GetPerimeter()
+        {
+            try
+            {
+                if (length <= 0 || width <= 0)
+                    throw new ArgumentException("Sides of a rectangle must be positive.");
+
+                return 2 * (length + width);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Error calculating perimeter: {ex.Message}");
+                return 0;
+            }
+        }
+
+        public override void Draw()
+        {
+            Console.WriteLine($"Drawing a {color.GetColor()} square  with length {length} and width {width}");
+        }
+    }
+}
---
+++ Bridge/Trushchak-03/Bridge/Bridge/Triangle.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Bridge
+{
+    class Triangle : Shape
+    {
+        private double side1;
+        private double side2;
+        private double side3;
+
+        public Triangle(double side1, double side2, double side3, IColor color) : base(color)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+            this.side3 = side3;
+        }
+
+        public override double GetArea()
+        {
+            try
+            {
+                
+                if (side1 <= 0 || side2 <= 0 || side3 <= 0)
+                    throw new ArgumentException("All sides of a triangle must be positive.");
+
+                
+                double semiPerimeter = (side1 + side2 + side3) / 2;
+                return Math.Sqrt(semiPerimeter * (semiPerimeter - side1) * (semiPerimeter - side2) * (semiPerimeter - side3));
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Error calculating area: {ex.Message}");
+                return 0;
+            }
+        }
+
+        public override double GetPerimeter()
+        {
+            try
+            {
+                
+                if (side1 <= 0 || side2 <= 0 || side3 <= 0)
+                    throw new ArgumentException("All sides of a triangle must be positive.");
+
+                return side1 + side2 + side3;
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Error calculating perimeter: {ex.Message}");
+                return 0;
+            }
+        }
+
+        public override void Draw()
+        {
+            Console.WriteLine($"Drawing a {color.GetColor()} triangle with sides {side1}, {side2}, {side3}");
+        }
+    }
+}
