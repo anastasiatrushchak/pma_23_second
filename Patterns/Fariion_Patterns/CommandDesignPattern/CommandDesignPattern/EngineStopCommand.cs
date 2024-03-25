3188edca83450def186b85125e923ca5df440667
Bohdan <108092227+bohdanfariyon@users.noreply.github.com>
2024-03-25 20:04:35 +0200

    FB-04 Patterns

---
+++ Patterns/Fariion_Patterns/CommandDesignPattern/CommandDesignPattern/EngineStopCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandDesignPattern_
+{
+    class EngineStopCommand : ICommand
+    {
+        private Car _car;
+
+        public EngineStopCommand(Car car)
+        {
+            _car = car;
+        }
+
+        public void Execute()
+        {
+            _car.StopEngine();
+        }
+    }
+}
---
+++ Patterns/Fariion_Patterns/FactoryMethod/FactoryMethod/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace FactoryMethod;
+
+
+class Program
+{
+    static void Client(CarFactory factory)
+    {
+        Car car = factory.CreateCar(); 
+        Console.WriteLine(car.Drive());
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Створення седана:");
+        Client(new SedanFactory());
+        Console.WriteLine();
+
+        Console.WriteLine("Створення кросовера:");
+        Client(new SUVFactory());
+    }
+}
