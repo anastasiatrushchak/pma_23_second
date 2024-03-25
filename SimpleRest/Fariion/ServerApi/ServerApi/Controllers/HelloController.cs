115f6cb53497338d39fecc9098c4ffa5a4ca8a93
Bohdan <108092227+bohdanfariyon@users.noreply.github.com>
2024-03-25 20:11:12 +0200

    FB-05 Simple Rest

---
+++ SimpleRest/Fariion/ServerApi/ServerApi/Controllers/HelloController.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+namespace ServerApi.Controllers
+
+{
+    [ApiController]
+    [Route("get")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetHello()
+        {
+            return Ok("Hello, World!");
+        }
+    }
+}
+
+
