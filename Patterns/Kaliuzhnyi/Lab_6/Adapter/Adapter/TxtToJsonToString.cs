d22d2566b09afce459f097a9c637fd1b4e9b0acd
Oleh911 <150347416+Oleh911@users.noreply.github.com>
2024-03-25 21:34:25 +0200

    KO-06
    KO - Lab #6 patterns

---
+++ Patterns/Kaliuzhnyi/Lab_6/Adapter/Adapter/TxtToJsonToString.cs
@@ -0,0 +1,28 @@
+﻿using Lab_6.Adapter.Adapter;
+using System.Text.Json;
+
+internal class TxtToJsonToString(string filename) : IJsonToString
+{
+    readonly string _filename = filename;
+
+    public string ConvertToString()
+    {
+        string[] lines = File.ReadAllLines(_filename);
+        if (lines.Length < 3)
+        {
+            throw new ArgumentException("Input file doesn't contain enough lines.");
+        }
+        string name = lines[0].Trim();
+        int age = int.Parse(lines[1].Trim());
+        string city = lines[2].Trim();
+        var jsonObject = new
+        {
+            name,
+            age,
+            city
+        };
+
+        return JsonSerializer.Serialize(jsonObject, new JsonSerializerOptions { WriteIndented = true });
+    }
+
+}
