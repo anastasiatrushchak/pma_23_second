555081677824d519748a25e9a87d9a02643c98aa
Zlatomyra <133869874+Zlatomyra@users.noreply.github.com>
2024-03-25 20:11:37 +0200

    ZS-04 patterns

---
+++ Patterns/Severylova-04/PatternBuilder/PatternBuilder/Builder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternBuilder
+{
+    public abstract class Builder
+    {
+        protected House house;
+        public abstract void SetColor();
+        public abstract void SetFloor();
+        public abstract void SetRoom();
+        public abstract void SetArea();
+
+        public void CreateHouse()
+        {
+            house = new House();
+        }
+
+        public House GetHouse()
+        {
+            return house;
+        }
+    }
+}
---
+++ Patterns/Severylova-04/PatternBuilder/PatternBuilder/HouseDirector.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternBuilder
+{
+    public class HouseDirector
+    {
+        public House MakeHouse(Builder builder)
+        {
+            builder.CreateHouse();
+            builder.SetColor();
+            builder.SetFloor();
+            builder.SetRoom();
+            builder.SetArea();
+
+            House house = builder.GetHouse();
+
+            if (house == null)
+            {
+                throw new InvalidOperationException("Builder failed to create a house.");
+            }
+
+            return house;
+
+        }
+    }
+}
