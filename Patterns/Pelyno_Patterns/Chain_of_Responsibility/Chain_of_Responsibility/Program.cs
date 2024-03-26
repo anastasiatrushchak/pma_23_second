98acd0d4545c04556cca237b0b411097262906b8
AnastasiaPelyno <143642188+AnastasiaPelyno@users.noreply.github.com>
2024-03-26 15:37:31 +0200

    Pelyno-04: Add patterns

---
+++ Patterns/Pelyno_Patterns/Chain_of_Responsibility/Chain_of_Responsibility/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+class HelpCenter
+{
+    public string RequestType { get; set; }
+
+    public HelpCenter(string requestType)
+    {
+        RequestType = requestType;
+    }
+}
+
+
+abstract class Handler
+{
+    public Handler next;
+
+    public void SetNext(Handler next)
+    {
+        this.next = next;
+    }
+
+    public abstract void HandleRequest(HelpCenter request);
+}
+
+
+class GasService : Handler
+{
+    public override void HandleRequest(HelpCenter request)
+    {
+        if (request.RequestType == "GasEmergency")
+        {
+            Console.WriteLine("Gas Service is handling the request.");
+        }
+        else if (next != null)
+        {
+            next.HandleRequest(request);
+        }
+    }
+}
+
+
+class FireService : Handler
+{
+    public override void HandleRequest(HelpCenter request)
+    {
+        if (request.RequestType == "FireEmergency")
+        {
+            Console.WriteLine("Fire Service is handling the request.");
+        }
+        else if (next != null)
+        {
+            next.HandleRequest(request);
+        }
+    }
+}
+
+
+class PoliceService : Handler
+{
+    public override void HandleRequest(HelpCenter request)
+    {
+        if (request.RequestType == "PoliceEmergency")
+        {
+            Console.WriteLine("Police Service is handling the request.");
+        }
+        else if (next != null)
+        {
+            next.HandleRequest(request);
+        }
+    }
+}
+
+
+class MedicalService : Handler
+{
+    public override void HandleRequest(HelpCenter request)
+    {
+        if (request.RequestType == "MedicalEmergency")
+        {
+            Console.WriteLine("Medical Service is handling the request.");
+        }
+        else if (next != null)
+        {
+            next.HandleRequest(request);
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Handler gasService = new GasService();
+        Handler fireService = new FireService();
+        Handler police = new PoliceService();
+        Handler medicalEmergency = new MedicalService();
+
+        
+        gasService.SetNext(medicalEmergency);
+        medicalEmergency.SetNext(fireService);
+        fireService.SetNext(police);
+
+        
+        HelpCenter request1 = new HelpCenter("GasEmergency");
+        HelpCenter request2 = new HelpCenter("MedicalEmergency");
+        HelpCenter request3 = new HelpCenter("FireEmergency");
+        HelpCenter request4 = new HelpCenter("PoliceEmergency");
+
+        
+        gasService.HandleRequest(request1);
+        gasService.HandleRequest(request4);
+        gasService.HandleRequest(request2);
+        gasService.HandleRequest(request3);
+    }
+}
---
+++ Patterns/Pelyno_Patterns/Proxy/Proxy/Database.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    class Database : IDatabase
+    {
+        public void UpdateData(object newData)
+        {
+            Console.WriteLine($"Updating data: {newData}");
+        }
+    }
+}
