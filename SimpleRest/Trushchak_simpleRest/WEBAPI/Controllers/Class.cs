11456986c72a1b0196d3fce09f8e4d2c89dea43c
anastasiatrushchak <143631657+anastasiatrushchak@users.noreply.github.com>
2024-03-26 21:33:01 +0200

    TA-05 SimpleRest

---
+++ SimpleRest/Trushchak_simpleRest/WEBAPI/Controllers/Class.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace WEBAPI.Controllers
+
+{
+    [ApiController]
+    [Route("get")]
+    public class Class : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetHello()
+        {
+            return Ok("Hello, World!");
+        }
+    }
+}
---
+++ SimpleRest/Trushchak_simpleRest/WEBAPI/Controllers/Headers.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace WEBAPI.Controllers
+{
+    public class Headers : ControllerBase
+    {
+        [HttpPost]
+        [Route("headers")]
+        public IActionResult GetHeaders()
+        {
+            if (Request.Headers.TryGetValue("Name", out StringValues name))
+            {
+                return Ok($"Hello, {name}!");
+            }
+            else
+            {
+                return Ok("Enter the name attribute in the Headers!");
+            }
+        }
+
+    }
+}
+
+
+
