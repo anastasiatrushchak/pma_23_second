b470343fbb4e606db5c3e5f3619bf031b3059ae1
Maria <123461037+KRMAR12@users.noreply.github.com>
2024-03-26 20:40:47 +0200

    Kravchuk-04: Add patterns

---
+++ Patterns/Kravchuk_Patterns/Fasad/Fasad/Fasad/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+class MathClass
+{
+    public void TeachMath()
+    {
+        Console.WriteLine("Teaching Math");
+    }
+}
+
+class LanguageClass
+{
+    public void TeachLanguage()
+    {
+        Console.WriteLine("Teaching Language");
+    }
+}
+
+class HistoryClass
+{
+    public void TeachHistory()
+    {
+        Console.WriteLine("Teaching History");
+    }
+}
+
+class SchoolFacade
+{
+    private MathClass mathClass;
+    private LanguageClass languageClass;
+    private HistoryClass historyClass;
+
+    public SchoolFacade()
+    {
+        mathClass = new MathClass();
+        languageClass = new LanguageClass();
+        historyClass = new HistoryClass();
+    }
+
+    public void TeachAllSubjects()
+    {
+        Console.WriteLine("Teaching all subjects...");
+        mathClass.TeachMath();
+        languageClass.TeachLanguage();
+        historyClass.TeachHistory();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SchoolFacade schoolFacade = new SchoolFacade();
+
+        schoolFacade.TeachAllSubjects();
+
+        Console.ReadLine();
+    }
+}
