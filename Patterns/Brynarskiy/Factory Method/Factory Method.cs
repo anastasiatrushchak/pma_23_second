e4b198d655d897800d3ecc2b6f0d878a908fa9ca
Andrriiy <143681105+Andrriiy@users.noreply.github.com>
2024-03-26 20:55:44 +0200

    BA-04

---
+++ Patterns/Brynarskiy/Factory Method/Factory Method.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+// Product interface
+interface IProduct
+{
+    void Operation();
+}
+
+// Concrete Product
+class ConcreteProduct : IProduct
+{
+    public void Operation()
+    {
+        Console.WriteLine("ConcreteProduct operation");
+    }
+}
+
+// Creator abstract class
+abstract class Creator
+{
+    public abstract IProduct FactoryMethod();
+}
+
+// Concrete Creator
+class ConcreteCreator : Creator
+{
+    public override IProduct FactoryMethod()
+    {
+        return new ConcreteProduct();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Client code
+        Creator creator = new ConcreteCreator();
+        IProduct product = creator.FactoryMethod();
+        product.Operation();
+    }
+}
