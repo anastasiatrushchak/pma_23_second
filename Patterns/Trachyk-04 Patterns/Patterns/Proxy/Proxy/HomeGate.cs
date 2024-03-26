e0cf36f19732ec7a55bb2984c9ce7882c4e67172
Yura-Maksym <143636132+Yura-Maksym@users.noreply.github.com>
2024-03-26 21:49:48 +0200

    TY-M-04 Patterns

---
+++ Patterns/Trachyk-04 Patterns/Patterns/Proxy/Proxy/HomeGate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class HomeGate : IHomeGate
+{
+    public void Open(string username, string password)
+    {
+        Console.WriteLine($"Opening gate for user: {username}");
+        // Реальна логіка відкриття воріт
+    }
+}
---
+++ Patterns/Trachyk-04 Patterns/Patterns/Singleton/Singleton/Singleton/SmartPhone.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    public class SmartPhone : Phone
+    {
+        private static SmartPhone? instance;
+        private static string? brand;
+
+        private SmartPhone(string brand)
+        {
+            SmartPhone.brand = brand;
+        }
+
+        public static SmartPhone GetInstance(string brand)
+        {
+            if (instance == null)
+            {
+                instance = new SmartPhone(brand);
+            }
+
+            return instance;
+        }
+
+        public override void Call(string number)
+        {
+            Console.WriteLine($"Calling {number} from {brand} smartphone");
+        }
+    }
+}
