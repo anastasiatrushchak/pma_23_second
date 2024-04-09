f1e47e4e5de2cb078a011706e7810598ed74becc
Maksym Petukhov <128901331+tellmant@users.noreply.github.com>
2024-04-09 17:32:37 +0300

    PM-06

---
+++ List/Petukhov/src/lnu/edu/ua/Node.java
@@ -0,0 +1,41 @@
+package lnu.edu.ua;
+
+public class Node<T> {
+
+    private T value;
+    private Node<T> prev;
+    private Node<T> next;
+
+    public Node(T value) {
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public Node<T> getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node<T> prev) {
+        this.prev = prev;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(value);
+    }
+}
