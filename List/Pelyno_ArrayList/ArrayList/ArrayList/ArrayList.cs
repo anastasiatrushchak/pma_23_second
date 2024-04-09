2e595073ee51c5ebe3be57ee53d07fc2b91e568c
AnastasiaPelyno <143642188+AnastasiaPelyno@users.noreply.github.com>
2024-04-09 20:13:18 +0300

    Pelyno-06:Add ArrayList

---
+++ List/Pelyno_ArrayList/ArrayList/ArrayList/ArrayList.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayList
+{
+    public class ArrayList<T>
+    {
+        private T[] array;
+        private int capacity;
+        private int size;
+
+        public ArrayList(T[] array)
+        {
+            this.capacity = array.Length;
+            this.size = array.Length;
+            this.array = array;
+            Resize();
+        }
+        public ArrayList(int capacity)
+        {
+            this.capacity = capacity;
+            this.size = 0;
+            this.array = new T[capacity];
+
+        }
+
+        public override string ToString()
+        {
+            return "[" + string.Join(", ", array[..size]) + "]";
+        }
+
+        private void Resize()
+        {
+            capacity = (int)(1.5 * capacity) + 1;
+            T[] newArray = new T[capacity];
+            Array.Copy(array, newArray, size);
+            array = newArray;
+        }
+
+        public void Add(T element)
+        {
+            if (size == capacity)
+            {
+                Resize();
+            }
+            array[size] = element;
+            size++;
+        }
+
+        public void Delete(int index)
+        {
+            if (index < 0 || index >= size)
+            {
+                Console.WriteLine("Invalid index. Deletion failed.");
+            }
+            else
+            {
+                for (int i = index; i < size - 1; i++)
+                {
+                    array[i] = array[i + 1];
+                }
+                array[size - 1] = default(T);
+                size--;
+            }
+        }
+
+        public void Insert(int index, T element)
+        {
+            if (size == capacity)
+            {
+                Resize();
+            }
+            for (int i = size; i > index; i--)
+            {
+                array[i] = array[i - 1];
+            }
+            array[index] = element;
+            size++;
+        }
+
+        public void Clear()
+        {
+            size = 0;
+            array = new T[capacity];
+        }
+
+        public void Printed()
+        {
+            Console.WriteLine("ArrayList: " + ToString());
+        }
+    }
+}
---
+++ List/Pelyno_ArrayList/ArrayList/ArrayList/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace ArrayList
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ArrayList<int> arrayList = new ArrayList<int>(8);
+            object[] arr = { 1, 28, 5, 12, 3, 4, 5, 4 };
+            foreach (int i in arr)
+            {
+                arrayList.Add(i);
+            }
+
+
+            Console.WriteLine("-------Original ArrayList-------");
+            arrayList.Printed();
+            Console.WriteLine("-------To ArrayList add 10 at the end-------");
+            arrayList.Add(10);
+            arrayList.Printed();
+            Console.WriteLine("-------From ArrayList delete element on 2 position-------");
+            try
+            {
+                arrayList.Delete(2);
+                arrayList.Printed();
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Invalid index. Deletion failed.");
+            }
+
+
+            Console.WriteLine("-------From ArrayList we try delete element on 58 position,which is not exist-------");
+            try
+            {
+                arrayList.Delete(58);
+
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Invalid index. Deletion failed.");
+            }
+            Console.WriteLine("-------To ArrayList we add element 18 on 0 position-------");
+            try
+            {
+                arrayList.Insert(0, 18);
+                arrayList.Printed();
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Invalid index or element. Insertion failed.");
+            }
+            Console.WriteLine("-------To ArrayList we try to add element 13 on 28 position, which is not exist-------");
+            try
+            {
+                arrayList.Insert(54, 13);
+                arrayList.Printed();
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Invalid index or element. Insertion failed.");
+            }
+            arrayList.Clear();
+            Console.WriteLine("-------ArrayList is cleared-------");
+            arrayList.Printed();
+        }
+    }
+}
