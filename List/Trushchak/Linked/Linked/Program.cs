f04da42c172491d69f24bee8533f69752c5aa522
anastasiatrushchak <143631657+anastasiatrushchak@users.noreply.github.com>
2024-04-09 20:51:09 +0300

    TA-06 : Add linked list

---
+++ List/Trushchak/Linked/Linked/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Linked
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> list = new LinkedList<int>();
+
+            list.Append(1);
+            list.Append(2);
+            list.Append(3);
+            list.Append(4);
+
+            Console.WriteLine("Original List:");
+            list.Display();
+
+            list.Insert(2, 10);
+            Console.WriteLine("List after Insertion:");
+            list.Display();
+
+            list.Remove(1);
+            Console.WriteLine("List after Removal:");
+            list.Display();
+        }
+    }
+}
