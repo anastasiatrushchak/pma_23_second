79393e58a817e8f9343230b2a7ac67cb81fec368
YuriiFlys <129283233+YuriiFlys@users.noreply.github.com>
2024-04-09 18:13:15 +0300

    FY: Add LinkedList

---
+++ List/Flys/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LinkedList
+{
+    public class Node<T>
+    {
+        public T Data { get; set; }
+        public Node<T> Next { get; set; }
+        public Node<T> Prev { get; set; }
+
+        public Node(T data)
+        {
+            this.Data = data;
+            this.Next = null;
+            this.Prev = null;
+        }
+    }
+}
