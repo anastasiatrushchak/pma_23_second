66b89b98212a2c8492ecee1ee9b8c2394349d7e6
Oleh911 <150347416+Oleh911@users.noreply.github.com>
2024-04-09 18:18:48 +0300

    KO-06
    Added lab #6

---
+++ List/Kaliuzhnyi-06/Lab_7/MyLinkedList.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Lab_7;
+
+public class MyLinkedList<T>
+{
+    private Node<T> _first;
+    private Node<T> _last;
+    private int _count;
+
+    public int Count { get { return _count; } }
+    public Node<T> First { get { return _first; } }
+    public Node<T> Last { get { return _last; } }
+
+    public MyLinkedList()
+    {
+        _first = null;
+        _last = null;
+        _count = 0;
+    }
+
+    public MyLinkedList(IEnumerable<T> values)
+    {
+        foreach (T value in values)
+        {
+            AddLast(value);
+        }
+    }
+
+    public T GetValue(int index)
+    {
+        if (index < 0 || index >= _count)
+        {
+            throw new ArgumentOutOfRangeException("Error! MyLinkedList.GetValue(int) index out of range!");
+        }
+        Node<T> currentNode = _first;
+        for (int i = 0; i < index; i++)
+        {
+            currentNode = currentNode.Next;
+        }
+
+        return currentNode.Value;
+    }
+    
+    public void AddLast(T value)
+    {
+        Node<T> newNode = new Node<T>(_last, value, _first);
+        if (_count == 0)
+        {
+            _first = newNode;
+            _last = newNode;
+            newNode.Next = newNode;
+            newNode.Previous = newNode;
+        }
+        else
+        {
+            _last.Next = newNode;
+            _first.Previous = newNode;
+            _last = newNode;
+        }
+        _count++;
+    }
+
+    public void AddFirst(T value)
+    {
+        Node<T> newNode = new Node<T>(_last, value, _first);
+        if (_count == 0)
+        {
+            _first = newNode;
+            _last = newNode;
+            newNode.Next = newNode;
+            newNode.Previous = newNode;
+        }
+        else
+        {
+            newNode.Next = _first;
+            newNode.Previous = _last;
+            _first.Previous = newNode;
+            _last.Next = newNode;
+            _first = newNode;
+        }
+        _count++;
+    }
+
+    public void Insert(int index, T value)
+    {
+        if (index < 0 || index >= _count)
+        {
+            throw new ArgumentOutOfRangeException("Error! MyLinkedList.Insert(int, T) index out of range!");
+        }
+
+        if (index == 0)
+        {
+            AddFirst(value);
+        }
+        else if (index == _count)
+        {
+            AddLast(value);
+        }
+        else
+        {
+            Node<T> currentNode = _first;
+            for (int i = 0; i < index - 1; i++)
+            {
+                currentNode = currentNode.Next;
+            }
+            Node<T> newNode = new Node<T>(currentNode, value, currentNode.Next);
+            currentNode.Next.Previous = newNode;
+            currentNode.Next = newNode;
+            _count++;
+        }
+    }
+
+    public void Clear()
+    {
+        _first = null;
+        _last = null;
+        _count = 0;
+    }
+
+    public bool Contains(T item)
+    {
+        Node<T> currentNode = _first;
+        for (int i = 0; i < _count; i++)
+        {
+            if (EqualityComparer<T>.Default.Equals(currentNode.Value, item))
+            {
+                return true;
+            }
+            currentNode = currentNode.Next;
+        }
+        return false;
+    }
+
+    public void Remove(T item)
+    {
+        Node<T> currentNode = _first;
+        for (int i = 0; i < _count; i++)
+        {
+            if (EqualityComparer<T>.Default.Equals(currentNode.Value, item))
+            {
+                RemoveAt(i);
+                return;
+            }
+            currentNode = currentNode.Next;
+        }
+        throw new InvalidOperationException("Item not found in the list");
+    }
+
+    public void RemoveAt(int index)
+    {
+        if (index < 0 || index >= _count)
+        {
+            throw new ArgumentOutOfRangeException("Error! MyLinkedList.RemoveAt(int) index out of range!");
+        }
+
+        if (_count == 1)
+        {
+            _first = null;
+            _last = null;
+        }
+        else if (index == 0)
+        {
+            _first.Next.Previous = _last;
+            _last.Next = _first.Next;
+            _first = _first.Next;
+        }
+        else if (index == _count - 1)
+        {
+            _last.Previous.Next = _first;
+            _first.Previous = _last.Previous;
+            _last = _last.Previous;
+        }
+        else
+        {
+            Node<T> currentNode = _first;
+            for (int i = 0; i < index; i++)
+            {
+                currentNode = currentNode.Next;
+            }
+            currentNode.Previous.Next = currentNode.Next;
+            currentNode.Next.Previous = currentNode.Previous;
+        }
+        _count--;
+    }
+
+    public override string ToString()
+    {
+        string toString = "";
+
+        Node<T> currentNode = _first;
+        for (int i = 0; i < _count; i++)
+        {
+            toString += $"({currentNode.Previous.Value}, {currentNode.Value}, {currentNode.Next.Value}) ";
+            currentNode = currentNode.Next;
+        }
+
+        return toString;
+    }
+}
+
+
---
+++ List/Kaliuzhnyi-06/Lab_7/Program.cs
@@ -0,0 +1,67 @@
+﻿using Lab_7;
+using System.Collections;
+using System.Runtime.CompilerServices;
+
+public static class Program
+{
+    public static void Main()
+    {
+        try
+        {
+            MyArrayList<int> arrayList = new(5);
+            for (int i = 0; i < 10; i++)
+            {
+                arrayList.Add(i);
+                Console.WriteLine(arrayList.ToString());
+            }
+            Console.WriteLine(arrayList.Contains(0));
+            Console.WriteLine(arrayList.Contains(100));
+            arrayList.Remove(0);
+            Console.WriteLine(arrayList);
+            arrayList.Clear();
+            arrayList.AddRange(new List<int> { 9, 8, 7, 6 });
+            Console.WriteLine(arrayList);
+            arrayList.AddRange(new int[] { 1, 2, 3, 4, 5 });
+            Console.WriteLine(arrayList);
+            arrayList.RemoveAt(0);
+            Console.WriteLine(arrayList);
+
+            Console.WriteLine("List test: ");
+            MyLinkedList<int> myList = new MyLinkedList<int>();
+
+            myList.AddLast(1);
+            myList.AddLast(2);
+            myList.AddLast(3);
+
+            Console.WriteLine("Initial list: " + myList);
+
+            Console.WriteLine("Value at index 1: " + myList.GetValue(1));
+
+            myList.AddFirst(0);
+            Console.WriteLine("List after adding 0 at the beginning: " + myList);
+
+            myList.Insert(2, 10);
+            Console.WriteLine("List after inserting 10 at index 2: " + myList);
+
+            myList.Clear();
+            Console.WriteLine("List after clearing: " + myList);
+
+            MyLinkedList<int> anotherList = new MyLinkedList<int>(new List<int> { 4, 5, 6 });
+            Console.WriteLine("Another list: " + anotherList);
+
+            Console.WriteLine("Does the list contain 5? " + anotherList.Contains(5));
+
+            anotherList.Remove(5);
+            Console.WriteLine("List after removing 5: " + anotherList);
+
+            anotherList.RemoveAt(1);
+            Console.WriteLine("List after removing element at index 1: " + anotherList);
+        
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+
+    }
+}
