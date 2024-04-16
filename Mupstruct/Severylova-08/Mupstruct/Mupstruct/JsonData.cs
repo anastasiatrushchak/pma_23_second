e8fd51d039f1941b8be519f448c834ad48341db0
Zlatomyra <133869874+Zlatomyra@users.noreply.github.com>
2024-04-16 21:23:02 +0300

    ZS-08: add mupstruct

---
+++ Mupstruct/Severylova-08/Mupstruct/Mupstruct/JsonData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mupstruct
+{
+    public class JsonData
+    {
+        public string IdentificatorId { get; set; }
+        public Item Item { get; set; }
+        public List<Info> Info { get; set; }
+    }
+}
