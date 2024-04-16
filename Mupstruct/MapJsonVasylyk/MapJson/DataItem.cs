4c83ecb3da86d2026dfd162afb1cd4161301a281
Nastia2004 <143537818+Nastia2004@users.noreply.github.com>
2024-04-16 22:15:29 +0300

    VA-09
    VA-09

---
+++ Mupstruct/MapJsonVasylyk/MapJson/DataItem.cs
@@ -0,0 +1,10 @@
+namespace MapJson;
+
+public class DataItem
+{
+    public string objectId{ get; set; }
+    public string effectimeFrom{ get; set; }
+    public string effectiveTo{ get; set; }
+    public string some_useless_stuff{ get; set; }
+    
+}
