00a00d48cd8559f8e552f9856486c775e021449a
AnastasiaPelyno <143642188+AnastasiaPelyno@users.noreply.github.com>
2024-04-16 21:56:05 +0300

    Pelyno_08-Add mapstruct

---
+++ Mupstruct/Pelyno_Mapstruct/JSON_TO_XML/JSON_TO_XML(class)/Info.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSON_XML
+{
+    public class Info
+    {
+        public string Season { get; set; }
+        public string checkInDate { get; set; }
+        public string checkOutDate { get; set; }
+        public string travelStyle { get; set; }
+        public string weatherForecast { get; set; }
+        public string type { get; set; }
+    }
+
+}
---
+++ Mupstruct/Pelyno_Mapstruct/JSON_TO_XML/JSON_TO_XML(class)/Vacation.cs
@@ -0,0 +1,18 @@
+﻿using JSON_XML;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSON_XML
+{
+    public class Vacation
+    {
+        public ObjectId? Id { get; set; }
+        public string identificatorId { get; set; }
+        public DataItem dataItem { get; set; }
+        public Info info { get; set; }
+    }
+}
