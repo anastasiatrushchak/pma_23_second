05254955a5e10d2cbd54f97fbe0562c65801a53a
Tusia1529 <143636956+Tusia1529@users.noreply.github.com>
2024-05-19 22:16:59 +0300

    KA-07: add Rest with database after term

---
+++ AfterTerm/Restwithdatabase.Khyshchenko/UserApi/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace UserApi
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+    
+
+    }
+}
